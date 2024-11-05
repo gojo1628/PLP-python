d4b30120c0d94975678fbf6dae757fdd0618249c
gojo1628 <ifeoluwaking074@gmail.com>
2024-11-05 21:04:31 +0100

    Create user_iput.py

---
+++ user_iput.py
@@ -0,0 +1,5 @@
+name = input("What is your name?: ").capitalize()
+age = input("How old are you? e.g '8 years old': ")
+location = input("What is the name of your Country?: ")
+
+print(f"Hello {name}! You are {age} and you live in {location}")
